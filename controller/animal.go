df2864e813824d72aa01195ae91ac94b31ee981b
Wendy <wendywibisono21@gmail.com>
2022-04-06 13:31:25 +0700

    Update

--- controller/animal.go
+++ controller/animal.go
@@ -16,12 +16,6 @@ type AnimalInput struct {
 	Legs  int16  `json:"legs" binding:"required"`
 }
 
-type AnimalUpdate struct {
-	Name  string `json:"name" binding:"required"`
-	Class string `json:"class"`
-	Legs  int16  `json:"legs"`
-}
-
 //Read Data
 func ReadData(c *gin.Context) {
 	db := c.MustGet("db").(*gorm.DB)
@@ -59,16 +53,16 @@ func UploadData(c *gin.Context) {
 		return
 	}
 
-	mhs := models.Animal{
+	anm := models.Animal{
 		Name:  dataInput.Name,
 		Class: dataInput.Class,
 		Legs:  dataInput.Legs,
 	}
 
-	db.Create(&mhs)
+	db.Create(&anm)
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Successfully insert data",
-		"Data":    mhs,
+		"Data":    anm,
 	})
 }
 
@@ -77,18 +71,58 @@ func UpdateData(c *gin.Context) {
 	db := c.MustGet("db").(*gorm.DB)
 
 	var animal models.Animal
-	var inputAnimal AnimalUpdate
+	var dataInput AnimalInput
 
-	if err := c.ShouldBindJSON(&inputAnimal); err != nil {
+	if err := c.ShouldBindJSON(&dataInput); err != nil {
+		errorMessages := []string{}
+		for _, e := range err.(validator.ValidationErrors) {
+			errorMessage := fmt.Sprintf("%s field is %s", e.Field(), e.ActualTag())
+			errorMessages = append(errorMessages, errorMessage)
+		}
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
+			"error": errorMessages,
 		})
 		return
 	}
 
-	db.Model(&animal).Update(&inputAnimal)
+	if err2 := db.Where("name = ?", dataInput.Name).Find(&animal).Error; err2 != nil {
+		anm := models.Animal{
+			Name:  dataInput.Name,
+			Class: dataInput.Class,
+			Legs:  dataInput.Legs,
+		}
+
+		db.Create(&anm)
+		c.JSON(http.StatusOK, gin.H{
+			"message": "No data found in database, so the program inserted the data to database",
+			"Data":    anm,
+		})
+
+	} else {
+
+		db.Model(&animal).Update(&dataInput)
+		c.JSON(http.StatusOK, gin.H{
+			"Message": "Successfull to Update Data",
+			"Data":    animal,
+		})
+	}
+}
+
+func DeleteData(c *gin.Context) {
+	db := c.MustGet("db").(*gorm.DB)
+	var animal models.Animal
+
+	a := c.Param("name")
+	if err := db.Where("name = ?", a).Find(&animal).Error; err != nil {
+		errorMessage := fmt.Sprintf("Data %s does not exist in database", a)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": errorMessage,
+		})
+		return
+	}
+
+	db.Delete(&animal)
 	c.JSON(http.StatusOK, gin.H{
-		"Message": "Successfull to Update Data",
-		"Data":    animal,
+		"Message": "Data successfully deleted",
 	})
 }
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"Github/BackendTasks/controller"
 	"Github/BackendTasks/models"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,14 +15,11 @@ func main() {
 		c.Set("db", db)
 		c.Next()
 	})
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "golang web api",
-		})
-	})
+
 	r.GET("/animal", controller.ReadData)
 	r.POST("/animal", controller.UploadData)
 	r.PUT("/animal", controller.UpdateData)
+	r.DELETE("/animal/:name", controller.DeleteData)
 
 	r.Run()
 }
